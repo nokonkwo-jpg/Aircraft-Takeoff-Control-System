eb0f18d420f201089ebefbccb6ef0dfb81d666f7
Nnaemeka <nokonkwo8426@gmail.com>
2025-11-11 23:03:50 -0400

    air_control_c is now compatible with ground control

--- air_control_c/src/functions.c
+++ air_control_c/src/functions.c
@@ -10,7 +10,7 @@
 #include "functions.h"
 
 #define SH_MEMORY_NAME "/air_traffic_shm"
-#define TOTAL_TAKEOFFS 50
+#define TOTAL_TAKEOFFS 20
 
 extern int planes;
 extern int takeoffs;
@@ -25,8 +25,6 @@ extern pid_t radio_pid;
 
 void MemoryCreate()
 {
-  // TODO2: create the shared memory segment, configure it and store the PID of
-  // the process in the first position of the memory block.
   int shm_fd = shm_open(SH_MEMORY_NAME, O_CREAT | O_RDWR, 0666);
   if (shm_fd == -1)
   {
@@ -58,15 +56,7 @@ void SigHandler2(int signal)
 
 void *TakeOffsFunction(void *arg)
 {
-  // TODO: implement the logic to control a takeoff thread
-  //    Use a loop that runs while total_takeoffs < TOTAL_TAKEOFFS
-  //    Use runway1_lock or runway2_lock to simulate a locked runway
-  //    Use state_lock for safe access to shared variables (planes,
-  //    takeoffs, total_takeoffs)
-  //    Simulate the time a takeoff takes with sleep(1)
-  //    Send SIGUSR1 every 5 local takeoffs
-  //    Send SIGTERM when the total takeoffs target is reached
-  while (total_takeoffs < TOTAL_TAKEOFFS)
+  while (1)
   {
     pthread_mutex_t *acquired_runway = NULL;
 
@@ -75,52 +65,57 @@ void *TakeOffsFunction(void *arg)
       if (pthread_mutex_trylock(&runway1_lock) == 0)
       {
         acquired_runway = &runway1_lock;
-        break;
       }
       else if (pthread_mutex_trylock(&runway2_lock) == 0)
       {
         acquired_runway = &runway2_lock;
-        break;
       }
       else
       {
-        usleep(1000); // Sleep before retrying
+        usleep(1000);
       }
     }
+
     pthread_mutex_lock(&state_lock);
+    
+    // Check if we're done
     if (total_takeoffs >= TOTAL_TAKEOFFS)
     {
       pthread_mutex_unlock(&state_lock);
       pthread_mutex_unlock(acquired_runway);
       break;
     }
+
+    // Check if there are planes available
     if (planes > 0)
     {
+      // Perform takeoff
       planes--;
       takeoffs++;
       total_takeoffs++;
-      if (takeoffs % 5 == 0)
+
+      // Check if we need to signal radio (every 5 takeoffs)
+      if (takeoffs >= 5)
       {
         kill(radio_pid, SIGUSR1);
         takeoffs = 0;
       }
 
-      int should_terminate = (total_takeoffs >= TOTAL_TAKEOFFS);
       pthread_mutex_unlock(&state_lock);
-      sleep(1); // Simulate takeoff time
+      
+      // Simulate takeoff time
+      sleep(1);
+      
+      // Release the runway
       pthread_mutex_unlock(acquired_runway);
-      if (should_terminate)
-      {
-        kill(radio_pid, SIGTERM);
-        break;
-      }
-      else
-      {
-        pthread_mutex_unlock(&state_lock);
-        pthread_mutex_unlock(acquired_runway);
-        usleep(100000); // Sleep for 100ms before retrying
-      }
+    }
+    else
+    {
+      pthread_mutex_unlock(&state_lock);
+      pthread_mutex_unlock(acquired_runway);
+      usleep(100000);
     }
   }
+  
   return NULL;
 }
--- air_control_c/src/main.c
+++ air_control_c/src/main.c
@@ -10,7 +10,7 @@
 #include "functions.h"
 
 #define SH_MEMORY_NAME "/air_traffic_shm"
-#define TOTAL_TAKEOFFS 50
+#define TOTAL_TAKEOFFS 20
 #define PLANES_LIMIT 20
 
 int planes = 0;
@@ -23,59 +23,50 @@ pthread_mutex_t runway2_lock = PTHREAD_MUTEX_INITIALIZER;
 
 int *shared_memory = NULL;
 pid_t radio_pid = 0;
+pid_t ground_pid = 0;
 
 int main()
 {
-  // TODO 1: Call the function that creates the shared memory segment.
   MemoryCreate();
-
-  // TODO 3: Configure the SIGUSR2 signal to increment the planes on the runway
-  // by 5.
+  
   struct sigaction sa;
   sa.sa_handler = SigHandler2;
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = 0;
-  if (sigaction(SIGUSR2, &sa, NULL) == -1)
-  {
-    perror("sigaction failed");
-    exit(1);
-  }
-  // TODO 4: Launch the 'radio' executable and, once launched, store its PID in
-  // the second position of the shared memory block.
-  radio_pid = fork();
-  if (radio_pid == -1)
-  {
-    perror("fork failed");
-    exit(1);
-  }
-  else if (radio_pid == 0)
-  {
-    execl("../../radio/build/./radio", "radio", SH_MEMORY_NAME, NULL);
-    perror("execl failed");
-    exit(1);
-  }
-  else
-  {
-    shared_memory[1] = radio_pid;
-    sleep(1);
-  }
+  sigaction(SIGUSR2, &sa, NULL);
 
-  // TODO 6: Launch 5 threads which will be the controllers; each thread will
-  // execute the TakeOffsFunction().
-  pthread_t controllers[5];
-  for (int i = 0; i < 5; i++)
-  {
-    if (pthread_create(&controllers[i], NULL, TakeOffsFunction, NULL) != 0)
-    {
-      perror("pthread_create failed");
-      exit(1);
-    }
+  radio_pid = fork();
+  if (radio_pid == 0) {
+    execl("../../radio/build/radio", "radio", SH_MEMORY_NAME, NULL);
+    perror("execl radio failed");
+    exit(1);
   }
-  for (int i = 0; i < 5; i++)
-  {
+  shared_memory[1] = radio_pid;
+  sleep(1);
+
+  ground_pid = fork();
+  if (ground_pid == 0) {
+    execl("../../ground_control/build/ground_control_c", "ground_control_c", SH_MEMORY_NAME, NULL);
+    perror("execl ground_control failed");
+    exit(1);
+  }
+  sleep(1); 
+
+  // Create 5 controller threads
+  pthread_t controllers[5];
+  for (int i = 0; i < 5; i++) {
+    pthread_create(&controllers[i], NULL, TakeOffsFunction, NULL);
+  }
+  
+  for (int i = 0; i < 5; i++) {
     pthread_join(controllers[i], NULL);
   }
-
+  
+  // Send SIGTERM to radio and ground_control
+  kill(radio_pid, SIGTERM);
+  kill(ground_pid, SIGTERM);
+  sleep(1);
+  
   munmap(shared_memory, 3 * sizeof(int));
   shm_unlink(SH_MEMORY_NAME);
   return 0;
